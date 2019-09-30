4794145e2abc6b4262b65959dc48e00e08971d5e
Kaito Sato <ksato.nit@gmail.com>
2019-09-30 17:46:51 +0900

    :sparkles: Add kLCM method

--- src/main/java/org/sysken/Util.java
+++ src/main/java/org/sysken/Util.java
@@ -15,4 +15,18 @@ public class Util {
         BigInteger N = A.multiply(B).divide(A.gcd(B));
         return N;
     }
+
+    /**
+     * kLCM メソッド
+     * 与えられた自然数kに対し，1, 2, ……, k すべての最小公倍数を返す．
+     * @param k kLCMを計算する上限．
+     * @return N 計算されたkLCM．
+     */
+    public static BigInteger kLCM(int k){
+        BigInteger N = new BigInteger("1");
+        for(int i = 1;i <= k;++i){
+            N = LCM(N, BigInteger.valueOf(i));
+        }
+        return N;
+    }
 }
