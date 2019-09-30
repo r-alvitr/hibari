b7d652838fc38743052222cfd52f46a68664fcbc
Kaito Sato <ksato.nit@gmail.com>
2019-09-30 16:02:05 +0900

    :sparkles: Add FactorizationProcessor class

---
+++ src/main/java/org/sysken/FactorizationProcessor.java
@@ -0,0 +1,18 @@
+package org.sysken;
+
+public class FactorizationProcessor {
+    public FactorizationProcessor(){
+
+    }
+
+    /**
+     * factor メソッド
+     * 与えられた自然数の非自明な約数を返す．
+     * @param N 分解したい合成数
+     * @return p Nの非自明な約数
+     */
+    public int factor(int N){
+        int p = 0;
+        return p;
+    }
+}
--- src/main/java/org/sysken/Main.java
+++ src/main/java/org/sysken/Main.java
@@ -2,6 +2,8 @@ package org.sysken;
 
 public class Main {
     public static void main(String[] args){
-
+        int N = 10;
+        FactorizationProcessor processor = new FactorizationProcessor();
+        processor.factor(N);
     }
 }
