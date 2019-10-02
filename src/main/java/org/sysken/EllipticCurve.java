5884cb57a221612e95947f2b2bc5a8d3a511c066
Kaito Sato <ksato.nit@gmail.com>
2019-10-02 22:24:40 +0900

    :hammer_and_wrench: Add new field

--- src/main/java/org/sysken/EllipticCurve.java
+++ src/main/java/org/sysken/EllipticCurve.java
@@ -7,6 +7,7 @@ public class EllipticCurve {
     private BigInteger N;
     private BigInteger b, c;
     private Point start;
+    public BigInteger discriminant;
 
     public EllipticCurve(BigInteger N, BigInteger b, Point p){
         this.N = N;
@@ -15,6 +16,6 @@ public class EllipticCurve {
         this.c = p.y.pow(2).subtract(p.x.pow(3).add(p.x.multiply(b))).mod(N); // cの値を逆算的に決める．
 
         //判別式を計算する．
-        BigInteger discriminant = b.pow(3).multiply(BigInteger.valueOf(4)).add( c.pow(2).multiply(BigInteger.valueOf(27))).gcd(N); // det = GCD(N, (4 b^2 + 27 c^3))
+        this.discriminant = b.pow(3).multiply(BigInteger.valueOf(4)).add( c.pow(2).multiply(BigInteger.valueOf(27))).gcd(N); // det = GCD(N, (4 b^2 + 27 c^3))
     }
 }
